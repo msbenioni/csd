759e036b7f8602cea01cb74ddebe1f13d3960a82
Jasmin <jasminb@missionreadyhq.com>
2024-11-01 11:43:33 +1300

    changed the colours for the calendar background & borders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,16 @@ import { Header } from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Providers } from "@/providers";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Clean Sweep Duo",
   description:
     "Family run rubbish removal service in South and East Auckland",
+  viewport: "width=device-width, initial-scale=1",
   icons: {
     icon: "/images/duofavicon.png",
   },
@@ -18,11 +23,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning className={inter.className}>
       <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <Providers>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
