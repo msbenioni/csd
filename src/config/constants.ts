7be79d2fccdc94e80ecfc2db472bc949232cd450
Jasmin <jasminb@missionreadyhq.com>
2024-10-31 23:20:23 +1300

    added a constants file

---
+++ src/config/constants.ts
@@ -0,0 +1,20 @@
+export const SITE_NAME = "Clean Sweep Duo";
+export const SITE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const BOOKING_CONSTANTS = {
+  MIN_BAGS: 1,
+  PRICE_PER_BAG: 8,
+  SERVICE_FEE: 6,
+};
+
+export const TIME_SLOTS = [
+  "7:00 AM - 8:00 AM",
+  "8:00 AM - 9:00 AM",
+  "9:00 AM - 10:00 AM",
+  "10:00 AM - 11:00 AM",
+  "11:00 AM - 12:00 PM",
+  "12:00 PM - 1:00 PM",
+  "1:00 PM - 2:00 PM",
+  "2:00 PM - 3:00 PM",
+  "3:00 PM - 4:00 PM",
+]; 
